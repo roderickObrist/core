96b0a63d211241913b1df74c8c2062dac3d5c53b
Roderick Obrist <roderick.obrist@permaconn.com>
2017-07-18 18:27:59 +1000

    replaced log

---
+++ log/index.js
@@ -0,0 +1,258 @@
+"use strict";
+
+const {config, is} = require("../index"),
+  write = require("./write"),
+  r = require("rethinkdb"),
+  os = require("os");
+
+require("colors");
+
+function makeBase(level, {connectionId = "", direction = "OUT", protocol = level, path = "", body = {}}) {
+  const base = {
+    level,
+    connectionId,
+    direction,
+    protocol,
+    path,
+    body,
+    "timestamp": r.now(),
+    "server": config.mode || os.hostname()
+  };
+
+  if (base.body === undefined) {
+    base.body = "undefined";
+  }
+
+  return base;
+}
+
+function safeStringify(base, format = null) {
+  let stringified = "";
+
+  try {
+    stringified = JSON.stringify(base.body, null, format);
+  } catch (e) {
+    if (!e.message.includes("circular structure")) {
+      throw e;
+    }
+
+    const map = new Map();
+
+    stringified = JSON.stringify(base.body, (key, value) => {
+      if (!(value instanceof Object)) {
+        return value;
+      }
+
+      if (is.func(value)) {
+        return `${value.constructor.name} ${value.name}()`;
+      }
+
+      if (map.has(value)) {
+        return map.get(value);
+      }
+
+      map.set(value, `[Circular->${key}]`);
+
+      return value;
+    }, format);
+
+    base.body = JSON.parse(stringified);
+  }
+
+  return stringified;
+}
+
+function isBase(obj) {
+  return obj &&
+    ["connectionId", "direction", "protocol", "path"].some(key => Object.hasOwnProperty.call(obj, key));
+}
+
+function formatMultipleArgs(all, level) {
+  let errorObject = all.find(item => item instanceof Error),
+    baseObject = all.find(isBase) || {},
+    {
+      direction = "IN",
+      protocol = "ERR",
+      path = ""
+    } = baseObject;
+
+  const {connectionId = "", body = {}} = baseObject;
+
+  if (!Object.keys(baseObject).length) {
+    baseObject = null;
+  }
+
+  if (errorObject) {
+    all.splice(all.indexOf(errorObject), 1);
+  } else {
+    errorObject = new Error();
+  }
+
+  if (baseObject) {
+    all.splice(all.indexOf(baseObject), 1);
+  }
+
+  all.filter(value => is.string(value))
+    .forEach(value => {
+      const i = all.indexOf(value);
+
+      all.splice(i, 1);
+
+      if (
+        is(value, "IN", "OUT")
+      ) {
+        direction = value;
+      } else if (errorObject.message === "") {
+        errorObject.message = value;
+      } else if (
+        value.length < 5 &&
+          value.toUpperCase() === value
+      ) {
+        protocol = value;
+      } else if (
+        !path &&
+          /[:/]/.test(value)
+      ) {
+        path = value;
+      } else {
+        if (!body.strings) {
+          body.strings = [];
+        }
+
+        body.strings.push(value);
+      }
+    });
+
+  body.stack = errorObject.stack.split("\n")
+    .filter((line, i) => i && !line.includes(__filename))
+    .map(line => line.trim());
+
+  ["query", "param"].forEach(key => {
+    if (errorObject[key]) {
+      body[key] = errorObject[key];
+    }
+  });
+
+  if (!body.code) {
+    body.code = errorObject.code || errorObject.message;
+  }
+
+  if (all.length === 1) {
+    if (baseObject) {
+      [body.extra] = all;
+    } else {
+      Object.assign(body, all[0]);
+    }
+  } else {
+    all.forEach((value, i) => {
+      body[i] = value;
+    });
+  }
+
+  return makeBase(level, {
+    connectionId,
+    direction,
+    protocol,
+    path,
+    body
+  });
+}
+
+exports.info = (data, optionalStringified) => {
+  const base = makeBase("info", data),
+    stringified = optionalStringified || safeStringify(base);
+
+  if (stringified.length > 512 * 1024) {
+    if (is.baseObject(base.body)) {
+      for (const [key, value] of Object.entries(base.body)) {
+        base.body[key] = is.baseObject(value)
+          ? "TRUNCATED"
+          : value;
+      }
+    } else {
+      base.body = "TRUNCATED";
+    }
+  }
+
+  write(base, stringified);
+};
+
+exports.dump = (...all) => {
+  const base = makeBase("info", {
+    "protocol": "DUMP",
+    "path": "debug-var",
+    "body": all.length > 1
+      ? all
+      : all[0]
+  });
+
+  exports.info(base, safeStringify(base));
+};
+
+exports.warn = (...all) => {
+  const base = formatMultipleArgs(all, "warn");
+
+  write(base, safeStringify(base, 2));
+
+};
+
+exports.error = (...all) => {
+  const base = formatMultipleArgs(all, "error");
+
+  //exports.dump(base);
+
+  write(base, safeStringify(base, 2));
+};
+
+
+exports.session = (details, stringified) => {
+  const {
+    connectionId = Math.random()
+      .toString(36)
+      .slice(-8),
+    direction = "OUT",
+    protocol = "INFO",
+    path = "none",
+    body = {}
+  } = details;
+
+  exports.info({
+    connectionId,
+    direction,
+    protocol,
+    path,
+    body
+  }, stringified);
+
+  const nextDirection = direction === "OUT"
+    ? "IN"
+    : "OUT";
+
+  return {
+    info(nextBody, nextStringified) {
+      exports.info({
+        connectionId,
+        protocol,
+        path,
+        "body": nextBody,
+        "direction": nextDirection
+      }, nextStringified);
+    },
+    warn(...all) {
+      exports.warn({
+        connectionId,
+        protocol,
+        path,
+        "direction": nextDirection
+      }, ...all);
+    },
+    error(...all) {
+      exports.error({
+        connectionId,
+        protocol,
+        path,
+        "direction": nextDirection
+      }, ...all);
+    }
+  };
+};
---
+++ log/write.js
@@ -0,0 +1,125 @@
+"use strict";
+
+const canHandleColors = process.stdout.isTTY,
+  {config, is} = require("../index"),
+  moment = require("moment"),
+  r = require("rethinkdb"),
+  noop = () => {};
+
+let store = noop,
+  print = noop;
+
+require("colors");
+
+module.exports = (data, stringified = JSON.stringify(data.body)) => {
+  store(data);
+  print(data, stringified);
+};
+
+(async rethinkdb => {
+  if (!rethinkdb) {
+    return;
+  }
+
+  let conn = null;
+
+  store = data => store.buffer.push(data);
+  store.buffer = [];
+
+  try {
+    conn = await r.connect(rethinkdb);
+  } catch (e) {
+    store = noop;
+    throw e;
+  }
+
+  let state = "connected";
+
+  async function insert(data) {
+    return r.db("log")
+      .table(moment().format("YYYY_MM_DD"))
+      .insert(data)
+      .run(conn, {
+        "durability": "soft"
+      });
+  }
+
+  // Empty the buffer
+  if (store.buffer.length) {
+    insert(store.buffer);
+  }
+
+  // Overwrite store
+  store = insert;
+
+  conn
+    .on("close", () => {
+      store = noop;
+
+      if (state === "connecting") {
+        return;
+      }
+
+      state = "connecting";
+
+      (async function connect() {
+        try {
+          await conn.reconnect();
+        } catch (err) {
+          setTimeout(connect, 1e3);
+        }
+      })();
+    })
+    .on("connect", () => {
+      state = "connected";
+      store = insert;
+    })
+    .on("timeout", () => {
+      store = noop;
+    })
+    .on("error", () => {
+      store = noop;
+    });
+})(config.rethinkdb);
+
+print = (data, stringified) => {
+  const t = new Date(),
+    maxStrLen = canHandleColors
+      ? process.stdout.columns - 19
+      : 200,
+    stringifiedCapped = stringified.length > maxStrLen
+      ? stringified.slice(0, maxStrLen)
+      : stringified;
+
+  if (is(data.level, "error", "warn")) {
+    const severity = data.level === "error"
+      ? "red"
+      : "yellow";
+
+    if (canHandleColors) {
+      console.error(`${data.body.code.bold[severity]} ${stringified}`);
+    } else {
+      console.error(`${data.path} ${stringified}`);
+    }
+
+    return;
+  }
+
+  if (canHandleColors) {
+    const time = t.toTimeString().substr(0, 8),
+      directionColour = data.direction === "IN"
+        ? "red"
+        : "green",
+      protoDirection = data.protocol.bold[directionColour],
+      connection = data.connectionId.slice(-3).blue;
+
+    console.log(`${time} ${protoDirection}[${connection}]: ${stringifiedCapped}`);
+    return;
+  }
+
+  const time = `${t.getDate()} ${t.toDateString().substr(4, 3)} ${t.toTimeString().substr(0, 8)}`,
+    protoConnectionDirection = `${data.protocol}: [${data.connectionId}] ${data.direction}`;
+
+  console.log(`${time} - ${protoConnectionDirection}: ${stringifiedCapped}`);
+};
+
